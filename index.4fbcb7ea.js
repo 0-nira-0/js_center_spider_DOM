9f59ee6e0ad79d5f0ee2b9f0eb5f7a66ed7f42f9
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-01-17 22:26:21 +0100

    make build

---
+++ index.4fbcb7ea.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.getBoundingClientRect(),n=t.getBoundingClientRect(),i=(c.height-n.height)/2,l=(c.width-n.width)/2;t.style.top="".concat(i,"px"),t.style.left="".concat(l,"px");
+//# sourceMappingURL=index.4fbcb7ea.js.map
